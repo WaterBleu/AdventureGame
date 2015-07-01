8cb2de0f10c2f1f6c67974118229cde1ccce94e8
WaterBleu <jeff7746@hotmail.com>
2015-07-01 12:07:58 -0700

    Initial Commit

---
+++ AdventureGame/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AdventureGame
+//
+//  Created by Jeff Huang on 2015-07-01.
+//  Copyright (c) 2015 Jeff Huang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
